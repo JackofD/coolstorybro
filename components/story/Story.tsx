b50cb36a70c13742e7bc9373f824fc96ce9f0aee
Deshen <deshen.padayachee@sqn.world>
2021-02-26 01:03:57 +0200

    Ficed layout of story

--- components/story/Story.tsx
+++ components/story/Story.tsx
@@ -8,11 +8,10 @@ type storyProps = {
 const Story: FC<storyProps> = (props) => {
   return <>
     <section id="story" className={`font-sans text-xl h-full max-h-full overflow-hidden ${props.className}`}>
-      <div className="overflow-y-auto min-h-full p-8">
-        <div className="min-h-full flex flex-row gap-4 justify-evenly content-center text-center">
+      <div className="overflow-y-auto min-h-full p-8 flex flex-row gap-4 justify-evenly items-center text-center">
+        {/* This will probably be conditionally rendered based on things */}
           <div>Story details</div>
           <div>Story inputs</div>
-        </div>
       </div>
     </section>
   </>;
