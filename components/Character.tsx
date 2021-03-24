4bbcb84c91f89e25f828f5564de0813107cf5aea
Deshen <deshen.padayachee@sqn.world>
2021-03-24 14:24:28 +0200

    Started defining data strcture

--- components/Character.tsx
+++ components/Character.tsx
@@ -9,9 +9,18 @@ type CharacterProps = {
 const Character: FC<CharacterProps> = (props) => {
   const { characterData: { initialDescriptor, secondaryDescriptor, tertiaryDescriptor, goal }, charIndex } = props;
 
+  const determineArticle = (word: string) => {
+    return /[aeiou]/.test(word.substring(0, 1)) ? "an" : "a";
+  };
+
+  const sentenceStarter = () => {
+    const article = determineArticle("obtuse");
+    return article.length > 1 ? article.slice(0, 1).toUpperCase() + article.slice(1) : article.slice(0, 1).toUpperCase();
+  };
+
   const formatCharacterData = () => {
     // format the data into a sentence/sentences with correct spacing and grammar (good luck figuring that out)
-
+    return `${sentenceStarter()} 'adjective-trait' 'adjective-descriptor' 'noun' 'initial-descriptor 'secondary-descriptor. 'Goal'.`;
   };
 
   const determineCharacterRole = (index: number) => {
@@ -24,6 +33,10 @@ const Character: FC<CharacterProps> = (props) => {
       case 1:
         charRole = 'Helper';
         break;
+
+      default:
+        charRole = 'Helper';
+        break;
     }
 
     return charRole
@@ -31,7 +44,7 @@ const Character: FC<CharacterProps> = (props) => {
 
   return <>
     <h4 className="mb-2 underline">{determineCharacterRole(charIndex)}:</h4>
-    <p className="mb-4">
+    <p className="mb-8">
       {`${initialDescriptor} ${secondaryDescriptor} ${tertiaryDescriptor}.${goal ? ' ' + goal : ' They have no goal in life'}`}
     </p>
   </>;
